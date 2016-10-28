b7e9f3500b3e13a3b6aea50da6a8e3e3104a944b
molecule <molecule@berkeley.edu>
2016-10-28 11:10:42 -0700

    Instead of raw readings, use IRRemote library v 1.0.0

--- sign-circuit/sign-circuit.ino
+++ sign-circuit/sign-circuit.ino
@@ -5,22 +5,8 @@ read an IR code and perform a function.
 Based on Adafruit tutorial http://learn.adafruit.com/ir-sensor/using-an-ir-sensor
 and http://learn.adafruit.com/trinket-gemma-mini-theramin-music-maker
 */
- 
-// We need to use the 'raw' pin reading methods because timing is very important here 
-// and the digitalRead() procedure is slower!
-#define IRrx_PIN_VECTOR PIND
-#define IRrx 5   // IR sensor - TSOP38238 on Pin GPIO #2 / D2
- 
-int led = 13; // blink 'digital' pin 1 - AKA the built-in red LED
 
-#define MAXPULSE    5000  // the maximum pulse we'll listen for - 5 milliseconds 
-#define NUMPULSES    50  // max IR pulse pairs to sample
-#define RESOLUTION     2  // // time between IR measurements
- 
-// we will store up to 50 pulse pairs (this is -a lot-, reduce if needed)
-uint16_t pulses[50][2];   // pair is high and low pulse
-uint16_t currentpulse = 0; // index for pulses we're storing
-uint32_t irCode = 0;
+#include <IRremote.h>
 
 //******** Neopixel ***********//
 #include <Adafruit_NeoPixel.h>
@@ -32,11 +18,15 @@ uint32_t irCode = 0;
 //   NEO_GRB     Pixels are wired for GRB bitstream
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
-#define NEOPIXEL_PIN 3
-#define NUM_LEDS 46
+#define NEOPIXEL_PIN 4
+#define NUM_LEDS 10
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t blue          = strip.Color(0, 0, 255);
+uint32_t leaf_green    = strip.Color(30, 44, 0);
+uint32_t hot_pink      = strip.Color(48, 0, 24);
+uint32_t yellowOrange  = strip.Color(44, 30, 0);
+
 int delayVal = 50;
 
 // Convenient 2D point structure
@@ -48,10 +38,31 @@ struct Point {
 float phase = 0.0;
 float phaseIncrement = 0.03;  // Controls the speed of the moving points. Higher == faster. I like 0.08 .
 float colorStretch = 0.11;    // Higher numbers will produce tighter color bands. I like 0.11 .
+
+// ******** IR ******** //
+const uint32_t IR_REMOTE_POWER     = 0x8322A15E;
+const uint32_t IR_REMOTE_SELECT    = 0x8322A659;
+const uint32_t IR_REMOTE_MUTE      = 0x8322AE51;
+const uint32_t IR_REMOTE_V_UP      = 0x8322A25D;
+const uint32_t IR_REMOTE_V_DOWN    = 0x8322A35C;
+const uint32_t IR_REMOTE_MODE      = 0x8322B24D;
+const uint32_t IR_REMOTE_REWIND    = 0x8322A55A;
+const uint32_t IR_REMOTE_PLAY      = 0x8322B04F;
+const uint32_t IR_REMOTE_FORWARD   = 0x8322A45B;
+const uint32_t HEADER              = 0x83220000;
+
+const uint32_t MASK = 0xFFFE0000;
+
+int RECV_PIN = 5;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
  
 void setup() {
   Serial.begin(9600);
-  Serial.println("raw_ir_receive");
+  Serial.println("IR-instructable-simple");
+  irrecv.enableIRIn(); // Start the receiver
 
   // Neopixel setup
   strip.begin();
@@ -60,159 +71,39 @@ void setup() {
  
 void loop() {
   
-  irCode=listenForIR(); // Wait for an IR Code
-  
-  // Process the pulses to get our code
-  for (int i = 0; i < 34; i++) {
-    irCode=irCode<<1;
-    if((pulses[i][0] * RESOLUTION)>0&&(pulses[i][0] * RESOLUTION)<500) {
-      irCode|=0; 
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    if (results.value == IR_REMOTE_SELECT) {
+      Serial.println("SELECT");
+      chase(hot_pink, false);
+      chase(blue, true);
+    }
+    else if (results.value == IR_REMOTE_PLAY) {
+      Serial.println("PLAY");
+      chase(leaf_green, false);
+      chase(blue, true);
+      
     } else {
-      irCode|=1;
+      Serial.println("OTHER BUTTON");
+      chase(yellowOrange, false);
+      chase(blue, true);
     }
-  }
-  Serial.println("irCode");
-  Serial.println(irCode, HEX);
 
-  //Check received IR code for comm header
-  uint32_t irCopy = irCode;
-  uint32_t received_header = irCopy>>25;
-  received_header = received_header << 1;
-  if (irCode != 0) {
-    for (int i = 0; i < 500; i++) {
-      neopixel_plasma();
-    }
-    chase();
-    cycle_color_flash(0, 0, 50);
+    irrecv.resume(); // Receive the next value
   }
 
-  memset(pulses, 0, sizeof(pulses));
+  
+
 } // end loop
  
 
-uint16_t listenForIR() {  // IR receive code
-  currentpulse = 0;
-  while (1) {
-   unsigned int highpulse, lowpulse;  // temporary storage timing
-   highpulse = lowpulse = 0; // start out with no pulse length 
-  
-   while (IRrx_PIN_VECTOR & _BV(IRrx)) { // got a high pulse
-      highpulse++; 
-      delayMicroseconds(RESOLUTION);
-      if (((highpulse >= MAXPULSE) && (currentpulse != 0)) || currentpulse == NUMPULSES) {
-        return currentpulse; 
-      }
-   }
-   pulses[currentpulse][0] = highpulse;
-
-   while (! (IRrx_PIN_VECTOR & _BV(IRrx))) { // got a low pulse
-      lowpulse++; 
-      delayMicroseconds(RESOLUTION);
-      if (((lowpulse >= MAXPULSE) && (currentpulse != 0))|| currentpulse == NUMPULSES) {
-        return currentpulse; 
-      }
-   }
-   pulses[currentpulse][1] = lowpulse;
-   currentpulse++;
-  }
-}
-
-static void chase() {
+static void chase(uint32_t color, boolean turnOff) {
   for(uint16_t i=0; i<strip.numPixels()+4; i++) {
-      strip.setPixelColor(i  , blue); // Draw new pixel
-      strip.setPixelColor(i-1, 0); // Erase pixel a few steps back
+      strip.setPixelColor(i  , color); // Draw new pixel
+      if (turnOff) {
+        strip.setPixelColor(i-1, 0); // Erase pixel a few steps back
+      }
       strip.show();
       delay(delayVal);
   }
-}
-
-void cycle_color_flash(int rStart, int gStart, int bStart) {
-  int Rstart, Gstart, Bstart = 0;
-
-  int Rend = rStart;
-  int Gend = gStart;
-  int Bend = bStart;
-  int n = 200;
-  for(int i = 0; i < n; i++) {// larger values of 'n' will give a smoother/slower transition.
-    for(int j = 0; j<strip.numPixels(); j++) {
-      float Rnew = Rstart + (Rend - Rstart) * i / n;
-      float Gnew = Gstart + (Gend - Gstart) * i / n;
-      float Bnew = Bstart + (Bend - Bstart) * i / n;
-      strip.setPixelColor(j, strip.Color(Rnew, Gnew, Bnew));
-    }
-    strip.show();
-    delay(10);
-  }  
-}
-
-/*
-    Multicolored Plasma for the Arduino Micro-Controller and NeoPixel Shield
-    Copyright (C) 2013 John Ericksen
-    https://github.com/johncarl81/neopixelplasma/blob/master/neopixelplasma.ino
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/.
-*/
-
-// This function is called every frame.
-void neopixel_plasma() {
-  phase += phaseIncrement;
-  
-  // The two points move along Lissajious curves, see: http://en.wikipedia.org/wiki/Lissajous_curve
-  // We want values that fit the LED grid: x values between 0..13, y values between 0..8 .
-  // The sin() function returns values in the range of -1.0..1.0, so scale these to our desired ranges.
-  // The phase value is multiplied by various constants; I chose these semi-randomly, to produce a nice motion.
-  Point p1 = { (sin(phase*1.000)+1.0) * 4.5, (sin(phase*1.310)+1.0) * 4.0 };
-  Point p2 = { (sin(phase*1.770)+1.0) * 4.5, (sin(phase*2.865)+1.0) * 4.0 };
-  Point p3 = { (sin(phase*0.250)+1.0) * 4.5, (sin(phase*0.750)+1.0) * 4.0 };
-  
-  byte row, col;
-  
-  // For each row...
-  for( row=0; row<5; row++ ) {
-    float row_f = float(row);  // Optimization: Keep a floating point value of the row number, instead of recasting it repeatedly.
-    
-    // For each column...
-    for( col=0; col<8; col++ ) {
-      float col_f = float(col);  // Optimization.
-      
-      // Calculate the distance between this LED, and p1.
-      Point dist1 = { col_f - p1.x, row_f - p1.y };  // The vector from p1 to this LED.
-      float distance1 = sqrt( dist1.x*dist1.x + dist1.y*dist1.y );
-      
-      // Calculate the distance between this LED, and p2.
-      Point dist2 = { col_f - p2.x, row_f - p2.y };  // The vector from p2 to this LED.
-      float distance2 = sqrt( dist2.x*dist2.x + dist2.y*dist2.y );
-      
-      // Calculate the distance between this LED, and p3.
-      Point dist3 = { col_f - p3.x, row_f - p3.y };  // The vector from p3 to this LED.
-      float distance3 = sqrt( dist3.x*dist3.x + dist3.y*dist3.y );
-      
-      // Warp the distance with a sin() function. As the distance value increases, the LEDs will get light,dark,light,dark,etc...
-      // You can use a cos() for slightly different shading, or experiment with other functions. Go crazy!
-      float color_1 = distance1;  // range: 0.0...1.0
-      float color_2 = distance2;
-      float color_3 = distance3;
-      float color_4 = (sin( distance1 * distance2 * colorStretch )) + 2.0 * 0.5;
-      
-      // Square the color_f value to weight it towards 0. The image will be darker and have higher contrast.
-      color_1 *= color_1 * color_4;
-      color_2 *= color_2 * color_4;
-      color_3 *= color_3 * color_4;
-      color_4 *= color_4;
- 
-      // Scale the color up to 0..7 . Max brightness is 7.
-      //strip.setPixelColor(col + (8 * row), strip.Color(color_4, 0, 0) );
-      strip.setPixelColor(col + (8 * row), strip.Color(color_1, color_2, color_3));
-      //strip.setPixelColor(col + (8 * row), strip.Color(color_1/4, color_2, color_3*5));
-    }
-  }
-  strip.show();
 }
